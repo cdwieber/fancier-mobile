c1899c9d6cd70c58e687e56abbee68f4f96bfcfa
Chris Wieber <chris.wieber@webdevstudios.com>
2018-11-19 16:19:37 -0500

    Auth workflow working

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -3,6 +3,7 @@ import { Text, AsyncStorage } from 'react-native';
 import { Container, Content, Form, Item, Input, Label, Button } from 'native-base';
 import { API_ROOT } from 'react-native-dotenv';
 import axios from 'axios';
+import Reactotron from 'reactotron-react-native'; 
 
 
 class SignIn extends React.Component {
@@ -16,24 +17,27 @@ class SignIn extends React.Component {
 
     _storeToken = async (response) => {
         try {
-            await AsyncStorage.setItem('userToken', JSON.stringify(response.data));
+            await AsyncStorage.setItem('user', JSON.stringify(response.data));
+            this.props.navigation.navigate('AuthLoading');
         } catch {
             console.log('ERROR. Unable to store token.');
         }
     }
 
-    login = (email, password) => {
+    login = () => {
+        Reactotron.log("BING BONG!");
         let payload={
-            "email":this.state.email,
-            "password":this.state.password
+             email: this.state.email,
+             password: this.state.password
             }
         const endpoint = API_ROOT + 'auth/login';
 
         axios.post(endpoint, payload)
         .then((response) => {
             this._storeToken(response)
+            Reactotron.log(response);
         }).catch((err) => {
-            console.log('API ERROR: ' + err);
+            Reactotron.log('API ERROR: ' + err);
         });
     }
 
@@ -44,16 +48,18 @@ class SignIn extends React.Component {
                 <Form>
                     <Item floatingLabel>
                     <Label>Email</Label>
-                    <Input 
-                    onChangeText={() => this.setState( {email: text})}/>
+                    <Input
+                    uppercase={false}
+                    onChangeText={(text) => this.setState( {email: text})}/>
                     </Item>
                     <Item floatingLabel last>
                     <Label>Password</Label>
-                    <Input 
-                    onChangeText={() => this.setState( { password: text })}/>
+                    <Input
+                    secureTextEntry={true}
+                    onChangeText={(text) => this.setState( { password: text })}/>
                     </Item>
                 </Form>
-                <Button block>
+                <Button block onPress={this.login}>
                     <Text>Sign In</Text>
                 </Button>
                 <Button block onPress={() => this.props.navigation.navigate('SignUp')}>
