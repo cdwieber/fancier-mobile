b93e37bebadd5ee3af2461990e9e2c6d88faf670
Chris Wieber <chris.wieber@webdevstudios.com>
2018-11-19 14:37:17 -0500

    SignIn to class component

--- src/Screens/SignIn.js
+++ src/Screens/SignIn.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const SignIn = () => {
-    return(
-        <View>
-            <Text>Hi! I'm a temporary sign-in screen placeholder.</Text>
-        </View>
-    );
+class SignIn extends React.Component {
+    render() {
+        return(
+            <View>
+                <Text>Hi! I'm a temporary sign-in screen placeholder.</Text>
+            </View>
+        );
+    }
 }
 
 export { SignIn };
